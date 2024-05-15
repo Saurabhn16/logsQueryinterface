03864115bc3d98791903a66f84ac6b4ad823cd8d
Saurabhn16 <nagpuresaurabh1610@gmail.com>
2024-05-15 15:27:21 +0530

    add backend and frontend

---
+++ interface/src/components/LogQueryResult.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const LogQueryResult = ({ result }) => {
+  if (!result || result.length === 0) {
+    return <p>No results found.</p>;
+  }
+
+  return (
+    <div className='LogQueryResult'>
+      <h2>Query Results</h2>
+      <ul>
+        {result.map((log) => (
+          <li key={log._id}>
+            <p>Level: {log.level}</p>
+            <p>Message: {log.message}</p>
+            <p>Timestamp: {formatTimestamp(log.timestamp)}</p>
+         
+    
+            {Object.keys(log.metadata).map((key) => (
+              <p key={key} className='metadata'>
+                {key}: {log.metadata[key]}
+              </p>
+            ))}
+
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+const formatTimestamp = (timestamp) => {
+  if (timestamp instanceof Date) {
+    return timestamp.toLocaleString();
+  }
+  return timestamp;
+};
+
+export default LogQueryResult;
